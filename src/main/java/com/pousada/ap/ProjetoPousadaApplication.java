5374f47a515a67b1576c10291980878f5f057fe9
unknown <mpss.frb@gmail.com>
2019-01-28 01:22:44 -0200

    Commit inicial dos arquivos do projeto.

---
+++ src/main/java/com/pousada/ap/ProjetoPousadaApplication.java
@@ -0,0 +1,14 @@
+package com.pousada.ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPousadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPousadaApplication.class, args);
+	}
+
+}
+
